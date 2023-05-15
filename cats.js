6ff14da59f230817e3ee01a3bdb8fa2cb3e6b16b
Anthony <anthonnhani@gmail.com>
2023-05-15 12:24:51 -0600

    added function that displays each cat's likes

---
+++ cats.js
@@ -0,0 +1,9 @@
+let cats = [{name: 'Tabby', likes: 'catnib'},
+            {name: 'Bob Ross', likes: 'paint'},
+            {name: 'Chaos', likes: 'Knocking plants over'}
+            ];
+for(let i = 0; i < cats.length; i++){
+    console.log(cats[i].likes);
+}
+
+    
